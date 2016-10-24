92e39bc9cb9520aed6b11d6980ee5da3f6952749
Michael Mulich <michael.mulich@gmail.com>
2016-10-24 16:32:06 -0700

    Add a function to convert a completezip to a litezip

---
+++ litezip/completezip.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from pathlib import Path
+
+from .main import parse_litezip
+
+
+__all__ = (
+)
+
+
+def convert_completezip(path):
+    """Converts a completezip file structure to a litezip file structure.
+    Returns a litezip data structure.
+
+    """
+    for filepath in path.glob('**/index_auto_generated.cnxml'):
+        filepath.rename(filepath.parent / 'index.cnxml')
+    return parse_litezip(path)
