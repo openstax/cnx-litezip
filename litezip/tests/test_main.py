c3039bd0fefdff459108ae6fb56f52d22a0af4f5
Michael Mulich <michael.mulich@gmail.com>
2016-10-24 10:42:46 -0700

    Add test for missing content file

--- litezip/tests/test_main.py
+++ litezip/tests/test_main.py
@@ -41,3 +41,16 @@ def test_parse_module_without_resources(datadir):
     assert data_struct[0] == module_id
     assert data_struct[1] == data_path / 'index.cnxml'
     assert data_struct[2] == tuple()
+
+
+def test_parse_module_raises_missing_file(tmpdir):
+    module_id = 'm42000'
+    data_path = Path(str(tmpdir.mkdir(module_id)))
+    missing_file = data_path / 'index.cnxml'
+
+    from litezip.main import parse_module
+    from litezip.exceptions import MissingFile
+    with pytest.raises(MissingFile) as exc_info:
+        parse_module(data_path)
+
+    assert missing_file == exc_info.value.args[0]
