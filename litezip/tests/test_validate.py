bb5f0381dd5b041b7fe6e0fbb9a5fa7043e1b0e5
Michael Mulich <michael.mulich@gmail.com>
2016-10-24 21:49:13 -0700

    Add function to validate the content using the cnxml library
    This pulls in the cnxml library to do the validation of the content.

--- litezip/tests/test_validate.py
+++ litezip/tests/test_validate.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import pytest
 
 
 def test_is_valid_identifier():
@@ -6,3 +7,23 @@ def test_is_valid_identifier():
     assert target('m40646')
     assert target('col11405')
     assert not target('mi5')
+
+
+def test_validate_collection(datadir):
+    from litezip.main import parse_collection
+    data_struct = parse_collection(datadir / 'litezip')
+
+    from litezip.validate import validate_content
+    errors = validate_content(data_struct)
+
+    assert not errors
+
+
+def test_validate_module(datadir):
+    from litezip.main import parse_module
+    data_struct = parse_module(datadir / 'litezip' / 'm40646')
+
+    from litezip.validate import validate_content
+    errors = validate_content(data_struct)
+
+    assert not errors
--- litezip/validate.py
+++ litezip/validate.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 import re
 
+import cnxml
+
 
 __all__ = (
     'is_valid_identifier',
+    'validate_content',
 )
 
 
@@ -13,3 +16,13 @@ VALID_ID_REGEX = re.compile("^(col\d{5,}\d*|m\d{4,})$")
 def is_valid_identifier(id):
     """Validate that the given `id`."""
     return VALID_ID_REGEX.match(id) is not None
+
+
+def validate_content(obj):
+    """Runs the correct validator for the given `obj`ect."""
+    from .main import Collection, Module
+    validator = {
+        Collection: cnxml.validate_collxml,
+        Module: cnxml.validate_cnxml,
+    }[type(obj)]
+    return validator(obj.file)
