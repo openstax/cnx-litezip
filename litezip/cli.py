ad0e2a09b6b536f1b73aedae86c04680f7721019
Michael Mulich <michael.mulich@gmail.com>
2016-10-24 18:10:43 -0700

    Allow the CLI to output to a separate directory

--- litezip/cli.py
+++ litezip/cli.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import argparse
+import shutil
 from pathlib import Path
 
 from litezip import convert_completezip
@@ -9,8 +10,12 @@ def _arg_parser():
     """Factory for creating the argument parser"""
     description = "Converts a completezip to a litezip"
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument('location',
-                        help="Location of the unpacked completezip")
+    parser.add_argument(
+        '-o', '--output-dir',
+        help="Location to output, rather than converting inplace")
+    parser.add_argument(
+        'location',
+        help="Location of the unpacked completezip")
     return parser
 
 
@@ -19,6 +24,12 @@ def completezip2litezip(argv=None):
     args = parser.parse_args(argv)
 
     completezip_path = Path(args.location)
+    output_dir = args.output_dir
+    if output_dir:
+        output_path = Path(output_dir)
+        shutil.copytree(str(completezip_path), str(output_path))
+        completezip_path = output_path
+
     struct = convert_completezip(completezip_path)
 
     return 0
