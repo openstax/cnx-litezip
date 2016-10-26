6f890137ccd1b6c388448a638f41d4d5e14bf107
Michael Mulich <michael.mulich@gmail.com>
2016-10-25 19:33:52 -0700

    Use logging for CLI output
    This will use the logging for CLI output/feedback. The logging lines
    will be a nice side effect procedure that can be used by the CLI
    code as well as later in the `press` & `berg` implementations.

--- litezip/cli.py
+++ litezip/cli.py
@@ -6,12 +6,22 @@ import sys
 from pathlib import Path
 
 from litezip import convert_completezip
+from litezip.logger import configure_logging, logger
 
 
 def _arg_parser():
     """Factory for creating the argument parser"""
     description = "Converts a completezip to a litezip"
     parser = argparse.ArgumentParser(description=description)
+    verbose_group = parser.add_mutually_exclusive_group()
+    verbose_group.add_argument(
+        '-v', '--verbose', action='store_true',
+        dest='verbose', default=None,
+        help="increase verbosity")
+    verbose_group.add_argument(
+        '-q', '--quiet', action='store_false',
+        dest='verbose', default=None,
+        help="print nothing to stdout or stderr")
     parser.add_argument(
         '-o', '--output-dir',
         help="Location to output, rather than converting inplace")
@@ -21,17 +31,59 @@ def _arg_parser():
     return parser
 
 
+console_logging_config = {
+    'version': 1,
+    'formatters': {
+        'cli': {
+            'format': '%(levelname)-5.5s: %(message)s',
+            },
+    },
+    'filters': {},
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'cli',
+            'filters': [],
+            'stream': 'ext://sys.stdout',
+        },
+    },
+    'loggers': {
+        'litezip': {
+            'level': 'ERROR',
+            'handlers': ['console'],
+            'propagate': 0,
+        },
+    },
+    'root': {
+        'level': 'NOTSET',
+        'handlers': [],
+    },
+}
+
+
+def set_verbosity(verbose):
+    config = console_logging_config.copy()
+    if verbose is None:
+        config['loggers']['litezip']['level'] = 'ERROR'
+    elif verbose:
+        config['loggers']['litezip']['level'] = 'DEBUG'
+    else:  # quiet
+        config['loggers']['litezip']['level'] = 100
+    configure_logging(config)
+
+
 def completezip2litezip(argv=None):
     parser = _arg_parser()
     args = parser.parse_args(argv)
+
+    set_verbosity(args.verbose)
 
     completezip_path = Path(args.location)
     output_dir = args.output_dir
     if output_dir:
         output_path = Path(output_dir)
         if output_path.exists():
-            print("ERROR: output-dir cannot exist prior to conversion",
-                  file=sys.stderr)
+            logger.error("output-dir cannot exist prior to conversion")
             return 1
         shutil.copytree(str(completezip_path), str(output_path))
         completezip_path = output_path
